1748c5928ada32d949c87331b09f0b200dcc76f2
Sri Harsha <sriharsha.g15@iiits.in>
2022-10-07 22:43:09 +0530

    Update admin.py

--- ratemytagline/admin.py
+++ ratemytagline/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import tagLine
+from .models import 
+# mention the app name that need to be added here
 
 #I am not able to see tagLine in the administrator console. Can you make it visible in the admin console?
