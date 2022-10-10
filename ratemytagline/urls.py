15aac35a1fa0eabf4e89fd916eb7d81e825425a2
Sri Harsha <sriharsha.g15@iiits.in>
2022-10-11 01:42:43 +0530

    Update urls.py

--- ratemytagline/urls.py
+++ ratemytagline/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
 from . import views
-#Alright alright , no bugs here. Move along >>>>
+#something seems not right. please fix it
 urlpatterns = [
     path('', views.index, name='index'),
-	path('taglines/<int:id>/', views.detail, name='detail'),
+	path('taglines/', views.detail, name='detail'),
 ]
